7ccdd89b61bf8ed2214ba27dded16e4ceda3a2a0
PratamaAngga <anggashillnoir@gmail.com>
2026-03-10 17:39:44 +0700

    Pembuatan fitur converter: function pengubah celcius ke satuan tujuan

--- Src/konversi_suhu/lib/converter.dart
+++ Src/konversi_suhu/lib/converter.dart
@@ -13,3 +13,25 @@ double keCelsius(double suhu, String satuan) {
       return suhu;
   }
 }
+
+// Langkah 2: Konversi dari Celsius ke satuan tujuan
+double dariCelsius(double celsius, String satuanTujuan) {
+  switch (satuanTujuan) {
+    case 'C':
+      return celsius;
+    case 'F':
+      return (celsius * 9 / 5) + 32;
+    case 'K':
+      return celsius + 273.15;
+    case 'R':
+      return celsius * 4 / 5;
+    default:
+      return celsius;
+  }
+}
+
+// Fungsi utama konversi: gabungin dua langkah di atas
+double konversiSuhu(double suhu, String satuanAsal, String satuanTujuan) {
+  final double celsius = keCelsius(suhu, satuanAsal);
+  return dariCelsius(celsius, satuanTujuan);
+}
