2bf9ea0720b47856bce3b9e40615efb35b68c57e
PratamaAngga <anggashillnoir@gmail.com>
2026-03-10 17:20:26 +0700

    Pembuatan fitur input validation: function input satuan suhu

--- Src/konversi_suhu/lib/input_handler.dart
+++ Src/konversi_suhu/lib/input_handler.dart
@@ -13,3 +13,19 @@ double getInputSuhu(String prompt) {
     print('Input tidak valid! Masukkan angka yang benar.');
   }
 }
+
+// Fungsi untuk mendapatkan pilihan satuan
+String getInputSatuan(String prompt) {
+  const List<String> satuanValid = ['C', 'F', 'K', 'R'];
+
+  while (true) {
+    stdout.write(prompt);
+    final String? input = stdin.readLineSync();
+    final String satuan = (input ?? '').toUpperCase().trim();
+
+    if (satuanValid.contains(satuan)) {
+      return satuan;
+    }
+    print('Satuan tidak valid! Pilih salah satu: C, F, K, R');
+  }
+}
